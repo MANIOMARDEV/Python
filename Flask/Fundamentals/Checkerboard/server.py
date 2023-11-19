34d8b2be00cee00cb455e18ebcce3f14b008ed9d
MANIOMARDEV <144290707+MANIOMARDEV@users.noreply.github.com>
2023-11-19 14:36:56 +0100

    first commit

--- Flask/Fundamentals/Checkerboard/server.py
+++ Flask/Fundamentals/Checkerboard/server.py
@@ -2,27 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return render_template("index.html",row=8,col=8,color_one='red',color_two='black')
-
+    return render_template("index.html", row=8, col=8, color_one='yellow', color_two='white', title='Index')
 
 @app.route('/<int:x>')
-def row(x):
-    return render_template("index.html",row=x,col=8,color_one='red',color_two='black')
+def row_route(x):
+    return render_template("index.html", row=x, col=8, color_one='yellow', color_two='white', title=f'Row {x}')
 
 @app.route('/<int:x>/<int:y>')
-def row_col(x,y):
-    return render_template("index.html",row=x,col=y,color_one='red',color_two='black')
+def row_col_route(x, y):
+    return render_template("index.html", row=x, col=y, color_one='yellow', color_two='white', title=f'Row {x}, Col {y}')
 
 @app.route('/<int:x>/<int:y>/<string:one>')
-def row_col_one(x,y,one):
-    return render_template("index.html",row=x,col=y,color_one=one,color_two='black')
+def row_col_one_route(x, y, one):
+    return render_template("index.html", row=x, col=y, color_one=one, color_two='white', title=f'Row {x}, Col {y}, Color {one}')
 
 @app.route('/<int:x>/<int:y>/<string:one>/<string:two>')
-def row_col_two(x,y,one,two):
-    return render_template("index.html",row=x,col=y,color_one=one,color_two=two)
+def row_col_two_route(x, y, one, two):
+    return render_template("index.html", row=x, col=y, color_one=one, color_two=two, title=f'Row {x}, Col {y}, Color {one} and {two}')
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
